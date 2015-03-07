52d81e940a2422fb53f08eeb2a173696c69e8d06
mockingbird2 <fabian.paul1@gmx.net>
2015-03-07 18:00:05 +0100

    removed starting implementation

--- richtigTanken/views.py
+++ richtigTanken/views.py
@@ -39,17 +39,3 @@ class UserPositionsViewSet(viewsets.ModelViewSet):
 
 def index(request):
     return render(request, 'richtigTanken/index.html')
-
-class SnippetList(APIView):
-
-    def get(self, request, formart=None):
-    snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
