729af7bd771f3ea28e88fe0a5e06447aef02b5a8
Alexander Immer <alexander.immer@yahoo.com>
2015-03-07 16:32:35 +0100

    Static connect testing

--- CodeFest8/settings.py
+++ CodeFest8/settings.py
@@ -82,6 +82,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = BASE_DIR + '/static/'
+
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
--- CodeFest8/urls.py
+++ CodeFest8/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^richtigTanken/', include('richtigTanken.urls')),
 ]
