26c99fd52b1bdc66cea3d497840f3c505ab61a28
Nico Ring <ringnico@web.de>
2015-03-07 15:24:19 +0100

    first layout

--- public/index.js
+++ public/index.js
@@ -1,3 +1,51 @@
-var test = $(".main");
+var homeTemplate = $("#homeTemplate").html();
+var mapTemplate = $("#mapTemplate").html();
 
-console.log(test);
+/* Circle Stuff*/
+var notificationCircleColor = "green";
+
+function showHome(data) {
+  showTemplate(homeTemplate, data);
+}
+
+function showMap(data) {
+  showTemplate(mapTemplate, data);
+}
+
+function showTemplate(template, data) {
+  $("#content").html(_.template(template, data));
+}
+
+showHome({});
+
+function changeCircleColor(color) {
+  var colorClass;
+  switch (color) {
+    case "green":
+      colorClass = "green";
+      break;
+    case "yellow":
+      colorClass = "yellow";
+      break;
+    case "red":
+      colorClass = "red";
+      break;
+    default:
+      return new Error("wrong color input.");
+  }
+
+  var $circle = $("#notification-circle");
+  $circle.removeClass(notificationCircleColor);
+  $circle.addClass(colorClass);
+  notificationCircleColor = colorClass;
+}
+
+function changeCircleMessage(text) {
+  var $circleMessage = $("#circle-message");
+  $circleMessage.text(text);
+}
+
+function changeCircleCount(count) {
+  var $circleCount = $("#circle-count");
+  $circleCount.text(count);
+}
