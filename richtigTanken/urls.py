1f526b4d3ca11ce0468dae504d296bf7e4ab0c07
Alexander Immer <alexander.immer@yahoo.com>
2015-03-07 21:17:05 +0100

    create Fahrtdaten

--- richtigTanken/urls.py
+++ richtigTanken/urls.py
@@ -4,5 +4,6 @@ from richtigTanken import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'newValue/', views.addWaypoint, name='location_update')
+    url(r'newValue/', views.addWaypoint, name='location_update'),
+    url(r'endRoute/', views.endRoute, name='stopRoute'),
 )
