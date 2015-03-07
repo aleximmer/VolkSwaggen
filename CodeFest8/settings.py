91582f15165133d663d0c6d1890d0416f89dfb34
Alexander Immer <alexander.immer@yahoo.com>
2015-03-07 12:39:24 +0100

    REST init

--- CodeFest8/settings.py
+++ CodeFest8/settings.py
@@ -85,9 +85,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
+    'PAGE_SIZE': 10
 }
--- richtigTanken/views.py
+++ richtigTanken/views.py
@@ -1,6 +1,19 @@
-from django.http import HttpResponse
-from django.template import RequestContext, loader
-from django.shortcuts import render_to_response
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from richtigTanken.serializers import UserSerializer, GroupSerializer #hieranders
 
-def index(request):
-    return render_to_response('richtigTanken/index.html')
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
