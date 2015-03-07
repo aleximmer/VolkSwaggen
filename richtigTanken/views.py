340e371a528973dad68c0f51fbdda62333917c12
mockingbird2 <fabian.paul1@gmx.net>
2015-03-07 17:58:46 +0100

    start implementing post and get

--- richtigTanken/views.py
+++ richtigTanken/views.py
@@ -5,6 +5,13 @@ from models import FahrtDaten, UserPositions
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -32,3 +39,17 @@ class UserPositionsViewSet(viewsets.ModelViewSet):
 
 def index(request):
     return render(request, 'richtigTanken/index.html')
+
+class SnippetList(APIView):
+
+    def get(self, request, formart=None):
+    snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
